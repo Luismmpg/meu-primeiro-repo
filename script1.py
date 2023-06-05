79d9068c449d667b26ee060e277d71e487136ed5
Luismmpg <luis.mmp.goncalves@gmail.com>
2023-06-05 17:19:11 +0100

    commit ao script 2

---
+++ script1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 24 22:41:24 2023
+
+@author: luism
+"""
+
+texto = input("Escreve aqui um texto ")
+print(texto)
---
+++ script2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 24 22:54:58 2023
+
+@author: luism
+"""
+
+nome_do_utilzador = input("Qual o seu nome? ")
+apelido_do_utilizador = input("Qual o seu apelido? ")
+
+print("O nome de utilzador é: "+nome_do_utilzador+" "+apelido_do_utilizador)
